7473263395f8ed2a5dba6f8a56d36a0ca8703c9e
Omar Badr <omarbadr938@gmail.com>
2024-02-14 14:32:52 +0100

    first commit

---
+++ Esercizio/HomePage.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Template.Master" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="Esercizio.HomePage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+            <h2 class="text-start">Vendita biglietti</h2>             
+                    <div class="mb-3">
+                        <label for="txtNome" class="form-label">Nome</label>
+                        <asp:TextBox ID="txtNome" CssClass="form-control" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="mb-3">
+                        <label for="txtCognome" class="form-label">Cognome</label>
+                        <asp:TextBox ID="txtCognome" CssClass="form-control" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="mb-3">
+                        <label for="ddlSala" class="form-label">Sala</label>
+                        <asp:DropDownList ID="ddlSala" CssClass="form-select" runat="server">
+                        </asp:DropDownList>
+                    </div>
+                    <div class="mb-3">
+                        <label for="ddlTipoBiglietto" class="form-label">Tipo Biglietto</label>
+                        <asp:DropDownList ID="ddlTipoBiglietto" CssClass="form-select" runat="server">
+                            <asp:ListItem Text="Intero" Value="Intero"></asp:ListItem>
+                            <asp:ListItem Text="Ridotto" Value="Ridotto"></asp:ListItem>
+                        </asp:DropDownList>
+                    </div>
+                    <asp:Button ID="btnSubmit" CssClass="btn btn-primary" runat="server" Text="Compra Biglietto" OnClick="btnSubmit_Click" />
+            </div>
+            <div class="col ">
+
+                <h2 class="text-start">Statistiche</h2>
+                <asp:Literal ID="listStatistiche" runat="server" />
+               
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Esercizio/HomePage.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Esercizio
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+
+            CaricaSale();
+            CaricaBigliettiVenduti();
+            }
+        }
+
+        private void CaricaSale()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["ConnessioneDBLocale"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT IDSala, NomeSala FROM Sale", conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    ddlSala.DataSource = reader;
+                    ddlSala.DataTextField = "NomeSala";
+                    ddlSala.DataValueField = "IDSala";
+                    ddlSala.DataBind();
+
+                }            
+            }
+        }
+
+        private void CaricaBigliettiVenduti()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["ConnessioneDBLocale"].ConnectionString;
+            StringBuilder statisticheHtml = new StringBuilder();
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                // Query per conteggio totale biglietti per sala
+                string querySale = "SELECT S.NomeSala, COUNT(B.IDBiglietto) AS TotaleBiglietti FROM Sale S LEFT JOIN Biglietti B ON S.IDSala = B.IDSala GROUP BY S.NomeSala";
+                using (SqlCommand cmdSale = new SqlCommand(querySale, conn))
+                {
+                    using (SqlDataReader readerSale = cmdSale.ExecuteReader())
+                    {
+                        while (readerSale.Read())
+                        {
+                            statisticheHtml.Append($"<li>Numero di biglietti venduti {readerSale["NomeSala"]}: {readerSale["TotaleBiglietti"]}</li>");
+                        }
+                    }
+                }
+
+                // Query per conteggio biglietti ridotti
+                string queryRidotti = "SELECT COUNT(*) AS TotaleRidotti FROM Biglietti WHERE TipoBiglietto = 'Ridotto'";
+                using (SqlCommand cmdRidotti = new SqlCommand(queryRidotti, conn))
+                {
+                    int totaleRidotti = (int)cmdRidotti.ExecuteScalar();
+                    statisticheHtml.Append($"<li>Numero di biglietti RIDOTTI venduti: {totaleRidotti}</li>");
+                }
+
+                // Assumi di avere un controllo Literal con ID litStatistiche per visualizzare le statistiche
+                listStatistiche.Text = statisticheHtml.ToString();
+            }
+        }
+
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+            string nome = txtNome.Text.Trim();
+            string cognome = txtCognome.Text.Trim();
+            int idSala = Convert.ToInt32(ddlSala.SelectedValue);
+            string tipoBiglietto = ddlTipoBiglietto.SelectedValue;
+            int capienzaMax = 120; // Capienza massima sala
+
+            string connString = ConfigurationManager.ConnectionStrings["ConnessioneDBLocale"].ConnectionString;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    // controllo numero attuale biglietti venduto per sala selezionata
+
+                    string queryConteggio = "SELECT COUNT(*) FROM Biglietti WHERE IDSala = @IDSala";
+
+                    SqlCommand cmdConteggio = new SqlCommand (queryConteggio, conn);
+                    cmdConteggio.Parameters.AddWithValue("@IDSala", idSala);
+                    int bigliettiVenduti = (int)cmdConteggio.ExecuteScalar();
+
+                    if(bigliettiVenduti >= capienzaMax)
+                    {
+                        Response.Write("La sala ha raggiunto la capienza massima. Non è possibile vendere altri biglietti");
+                        return;
+                    }
+
+                    string query = "INSERT INTO Biglietti (Nome, Cognome, IDSala, TipoBiglietto ) VALUES (@Nome, @Cognome, @IDSala, @TipoBiglietto)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn) )
+                    {
+                        cmd.Parameters.AddWithValue("@Nome", nome);
+                        cmd.Parameters.AddWithValue("@Cognome", cognome);
+                        cmd.Parameters.AddWithValue("@IDSala", idSala);
+                        cmd.Parameters.AddWithValue("@TipoBiglietto", tipoBiglietto);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                Response.Redirect(Request.RawUrl); // reload the page
+
+            } catch (Exception ex) {
+
+                Response.Write($"Si è verificato un errore: {ex.Message}");
+            }
+
+
+            CaricaBigliettiVenduti();
+        }
+    }
+}
